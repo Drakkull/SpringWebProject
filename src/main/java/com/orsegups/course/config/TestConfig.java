76babb01d183201f84c1c6c9b732e46153abd294
Leonardo DiAbreu <drakkull007@hotmail.com>
2023-06-01 11:04:38 -0300

    Join table and many to many done - still working

--- src/main/java/com/orsegups/course/config/TestConfig.java
+++ src/main/java/com/orsegups/course/config/TestConfig.java
@@ -45,6 +45,9 @@ public class TestConfig implements CommandLineRunner {
         Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
 
+
+
+
         User u1 = new User(null, "Kratos", "klebi@gmail.com", "66666", "xast1");
         User u2 = new User(null, "Pit", "pituca@gmail.com", "2345678", "1234");
 
@@ -56,5 +59,18 @@ public class TestConfig implements CommandLineRunner {
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
         productRepository.saveAll(Arrays.asList(p1,p2,p3, p4, p5));
+
+
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+
+
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
     }
 }
